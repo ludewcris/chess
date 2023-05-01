84cd4f3d5b92a39433c9c5c6cacf01df706f7f72
ludewcris <ludewcris@gmail.com>
2023-05-01 21:53:27 +0200

    0.0.2: Assets replacement.

--- chessboard/src/assets/pieces/index.js
+++ chessboard/src/assets/pieces/index.js
@@ -1,13 +1,13 @@
-export { default as wP } from "./pawn_w.png";
-export { default as wR } from "./rook_w.png";
-export { default as wN } from "./knight_w.png";
-export { default as wB } from "./bishop_w.png";
-export { default as wQ } from "./queen_w.png";
-export { default as wK } from "./king_w.png";
+export { default as wP } from "./pawn_w.svg";
+export { default as wR } from "./rook_w.svg";
+export { default as wN } from "./knight_w.svg";
+export { default as wB } from "./bishop_w.svg";
+export { default as wQ } from "./queen_w.svg";
+export { default as wK } from "./king_w.svg";
 
-export { default as bP } from "./pawn_b.png";
-export { default as bR } from "./rook_b.png";
-export { default as bN } from "./knight_b.png";
-export { default as bB } from "./bishop_b.png";
-export { default as bQ } from "./queen_b.png";
-export { default as bK } from "./king_b.png";
+export { default as bP } from "./pawn_b.svg";
+export { default as bR } from "./rook_b.svg";
+export { default as bN } from "./knight_b.svg";
+export { default as bB } from "./bishop_b.svg";
+export { default as bQ } from "./queen_b.svg";
+export { default as bK } from "./king_b.svg";
